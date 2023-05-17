1f19b888430a5ee3251227658212a53f21b66809
Maurix <125933942+Maurix-Phoenix@users.noreply.github.com>
2023-05-17 13:16:13 +0200

    Initial Commit

---
+++ AdventureGame/Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+
+    private void Awake()
+    {
+
+    }
+
+    private void OnEnable()
+    {
+        
+    }
+    private void OnDisable()
+    {
+        
+    }
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+
+    }
+
+
+}
+
---
+++ AdventureGame/Assets/Scripts/StartingVillage.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartingVillage : MonoBehaviour
+{
+    public Player Player;
+    public Transform PlayerStartingPoint;
+
+    private void Awake()
+    {
+        PlayerStartingPoint = GameObject.Find("PlayerStartingPoint").transform;
+    }
+
+    void Start()
+    {
+        SpawnPlayer();
+    }
+
+   
+    void Update()
+    {
+        
+    }
+
+    void SpawnPlayer()
+    {
+        Player.gameObject.SetActive(true);
+    }
+}
